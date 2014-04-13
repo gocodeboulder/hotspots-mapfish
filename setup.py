7a799514b684cf7ada442fd94a886fe27e63b34f
barryloper <barry@loper.cc>
2014-04-13 16:30:52 -0600

    hello world app

---
+++ setup.py
@@ -0,0 +1,37 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='hotspots',
+    version='0.1',
+    description='',
+    author='',
+    author_email='',
+    url='',
+    install_requires=[
+        "Pylons>=1.0",
+        "SQLAlchemy>=0.5",
+        "Genshi>=0.4",
+    ],
+    setup_requires=["PasteScript>=1.6.3"],
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    test_suite='nose.collector',
+    package_data={'hotspots': ['i18n/*/LC_MESSAGES/*.mo']},
+    #message_extractors={'hotspots': [
+    #        ('**.py', 'python', None),
+    #        ('public/**', 'ignore', None)]},
+    zip_safe=False,
+    paster_plugins=['MapFish', 'PasteScript', 'Pylons'],
+    entry_points="""
+    [paste.app_factory]
+    main = hotspots.config.middleware:make_app
+
+    [paste.app_install]
+    main = pylons.util:PylonsInstaller
+    """,
+)
