9768f20c704c461ed77b09ede515de7a5bba1eb7
Barry Loper <barry@gocode.(none)>
2014-05-02 22:04:37 -0400

    added hotspots front-end

---
+++ hotspots/public/js/leaflet-code.js
@@ -0,0 +1,41 @@
+var map = new L.Map('mapDiv', {center: new L.LatLng(39.825413,-105.056763), zoom: 10});
+var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+var styles = [
+    {
+      featureType: 'all',
+      stylers: [{hue: '#ff0000'}]
+    }
+ ];
+
+var ggl = new L.Google('ROADMAP', {
+	mapOptions: {
+		styles: styles
+	}
+});
+
+map.addLayer(ggl);
+map.addControl(new L.Control.Layers( {'OSM':osm, 'Google':ggl}, {}));
+
+var cycle = L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {
+            attribution: '© OpenCycleMap contributors - © OpenStreetMap contributors'
+            , opacity: 1
+        })
+map.addLayer(cycle);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+var zonesLayer = L.geoJson().addTo(map); //get the geojson from mapfish localhost:5000
+$.get('/zones',
+    {'limit': 1000},
+    function(data){zonesLayer.addData(data);},
+    'json');
+map.addLayer(zonesLayer);
+map.on('click', onMapClick);
